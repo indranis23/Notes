75002372467ee72f23dc5536bc3f8707320f8e50
Indrani sarkar <indranis2307@gmail.com>
2020-09-16 21:36:29 +0530

    Updates

---
+++ precache-manifest.72e4f01dd95ae4fa462b76b7951dda41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "939a211d8015f1f7899490207a515062",
+    "url": "/Notes/index.html"
+  },
+  {
+    "revision": "3e0260af8213d7c2f3ce",
+    "url": "/Notes/static/js/2.cdc8e013.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Notes/static/js/2.cdc8e013.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "621e67f8159df55b0585",
+    "url": "/Notes/static/js/main.73186053.chunk.js"
+  },
+  {
+    "revision": "5145846761549db7bb08",
+    "url": "/Notes/static/js/runtime-main.19664af7.js"
+  }
+]);
